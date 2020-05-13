aab143a9c875eaa7577fefac7211b1fa354f0a3a
fikri <fikrihelmi17@gmail.com>
2020-05-13 08:09:13 +0700

    Initial Commit

---
+++ MemberDicoding/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MemberDicoding
+//
+//  Created by Fikri on 13/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
